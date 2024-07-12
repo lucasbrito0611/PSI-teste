2746e2f9ab36afb01fbfcf6f701797b67efd6110
brito <brito@gmail.com>
2024-07-12 11:47:26 -0300

    Adicionando barra de pesquisa e data

--- loja/admin.py
+++ loja/admin.py
@@ -5,12 +5,18 @@ from .models import *
 
 class FabricanteAdmin(admin.ModelAdmin):
     date_hierarchy = 'criado_em'
+    search_fields = ('Fabricante',)
+
+class CategoriaAdmin(admin.ModelAdmin):
+    date_hierarchy = 'criado_em'
+    search_fields = ('Categoria',)
 
 class ProdutoAdmin(admin.ModelAdmin):
     date_hierarchy = 'criado_em'
     list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao', 'preco', 'categoria',)
     empty_value_display = 'Vazio'
+    search_fields = ('Produto',)
 
 admin.site.register(Fabricante, FabricanteAdmin) 
-admin.site.register(Categoria)
+admin.site.register(Categoria, CategoriaAdmin)
 admin.site.register(Produto, ProdutoAdmin)
