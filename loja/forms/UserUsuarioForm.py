e23454cf1d44b56f7c17e2fe16cee8f631683fc6
lucasbrito0611 <brito.s@escolar.ifrn.edu.br>
2024-10-21 10:27:37 -0300

    Continuação do capítulo 9

--- loja/forms/UserUsuarioForm.py
+++ loja/forms/UserUsuarioForm.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from django import forms
+from django.contrib.auth.models import User
 from loja.models.Usuario import Usuario
 
 class UserUsuarioForm(ModelForm):
@@ -13,3 +14,14 @@ class UserUsuarioForm(ModelForm):
         model = Usuario
         fields = ['user', 'perfil', 'aniversario']
         widgets = {'user': forms.HiddenInput(), 'perfil': forms.Select(attrs={'class': "form-control"}), 'aniversario': forms.DateInput(attrs={'class':"form-control", "type": "date"})}
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': "form-control"}),
+            'email': forms.EmailInput(attrs={'class': "form-control"}),
+            'first_name': forms.TextInput(attrs={'class': "form-control"}),
+            'last_name': forms.TextInput(attrs={'class': "form-control"})
+        }
