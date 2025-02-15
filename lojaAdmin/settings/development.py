a1ea9ab6cd26575c3a5e4ed541b7bec63f4de73a
lucasbrito0611 <lucasbrito2006@hotmail.com>
2025-02-15 15:21:56 -0300

    erro

--- lojaAdmin/settings/development.py
+++ lojaAdmin/settings/development.py
@@ -4,9 +4,14 @@ DEBUG = True
 #Crie secret key para seu ambiente de desenvolvimento
 SECRET_KEY='ixb62ha#ts=ab4t2u%p1_62-!5w2j==j6d^3-j$!z(@*m+-h'
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
-DATABASES={
-'default':{
-'ENGINE':'django.db.backends.sqlite3',
-'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neondb',
+        'USER': 'neondb_owner',
+        'PASSWORD': 'npg_XWL8oQ9MJCvl',
+        'HOST': 'ep-sparkling-rain-a8v31umy-pooler.eastus2.azure.neon.tech',
+        'PORT': '5432',
+        'OPTIONS': {'sslmode': 'require'},
+    }
 }
--- lojaAdmin/settings/production.py
+++ lojaAdmin/settings/production.py
@@ -5,8 +5,13 @@ DEBUG = False
 SECRET_KEY = 'ixb6fha#ts=&b4t2u%p1_62-!8dw2j==j)d^3-j$!z(@*m+-h'
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 DATABASES = {
-'default':{
-'ENGINE':'django.db.backends.sqlite3',
-'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-}
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neondb',
+        'USER': 'neondb_owner',
+        'PASSWORD': 'npg_XWL8oQ9MJCvl',
+        'HOST': 'ep-sparkling-rain-a8v31umy-pooler.eastus2.azure.neon.tech',
+        'PORT': '5432',
+        'OPTIONS': {'sslmode': 'require'},
+    }
 }
