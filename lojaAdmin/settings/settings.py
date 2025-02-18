81c4e50effe10e7baf74368bc9312f4531d93bda
lucasbrito0611 <lucasbrito2006@hotmail.com>
2025-02-17 21:37:50 -0300

    Ajeitando o erro do css

--- lojaAdmin/settings/settings.py
+++ lojaAdmin/settings/settings.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles_build')
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR.parent, 'loja', 'static')
